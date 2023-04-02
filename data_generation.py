5cf5dc427a500c5445e89ba7e568fdbc58ba20bf
AFilonov <filon13p@gmail.com>
2023-04-02 13:14:28 +0300

    вынес генерацию в модуль

--- data_generation.py
+++ data_generation.py
@@ -1,32 +1,51 @@
 import random
+import numpy as np
+import math
 
 
 class DataPoint:
-    def __init__(self, x, y):
+    def __init__(self, x, y, class_type=0):
         self.x = x
         self.y = y
+        self.class_type = class_type
 
+    def __str__(self):
+        return "(" + str(self.x) + ":" + str(self.y) + ")"
+
+    def __repr__(self):
+        return str(self)
 
 def create_data(amount_of_points, func_y, interval_start_x=0, interval_end_x=100):
     array = []
-    for i in range(0, amount_of_points):
-        x = random.randint(interval_start_x, interval_end_x)
-        array.append(DataPoint(x, func_y(x)))
+
+    array += create_circle_of_points(int(amount_of_points / 2), -1, 70, 70, 25)
+    array += create_circle_of_points(int(amount_of_points / 2), 1, 30, 30, 25)
+
     return array
 
 
-def normalize(points):
+def create_circle_of_points(amount_of_points, type_class, circle_x, circle_y, circle_r):
     array = []
-    max_x = 100
-    min_x = 0
-    max_y = 100
-    min_y = 0
-    for point in points:
-        array.append(
-            DataPoint(
-                (point.x - min_x) / max_x,
-                (point.y - min_y) / max_y,
-            )
-        )
+    for i in range(0, int(amount_of_points)):
+        point = creat_point_in_circle(circle_x, circle_y, circle_r)
+        point.class_type = type_class
+        array.append(point)
+    return array
 
-    return points
+
+def creat_point_in_circle(circle_x, circle_y, circle_r):
+    # random angle
+    alpha = 2 * math.pi * np.random.random()
+    # random radius
+    r = circle_r * math.sqrt(np.random.random())
+    # calculating coordinates
+    x = r * math.cos(alpha) + circle_x
+    y = r * math.sin(alpha) + circle_y
+    return DataPoint(x, y)
+
+
+def create_point_around_y_line(x, func_y):
+    disp = np.random.uniform(-1, 1) + 1
+    return DataPoint(x, disp * func_y(x))
+
+
--- main.py
+++ main.py
@@ -1,58 +1,10 @@
-# from data_generation import *
+from data_generation import *
 import matplotlib.pyplot as plt
-import numpy as np
-import math
 
 
 def linear_func_y(x):
     return -100 * x + 1
 
-
-class DataPoint:
-    def __init__(self, x, y, class_type=0):
-        self.x = x
-        self.y = y
-        self.class_type = class_type
-
-    def __str__(self):
-        return "(" + str(self.x) + ":" + str(self.y) + ")"
-
-    def __repr__(self):
-        return str(self)
-
-
-def creat_point_in_circle(circle_x, circle_y, circle_r):
-    # random angle
-    alpha = 2 * math.pi * np.random.random()
-    # random radius
-    r = circle_r * math.sqrt(np.random.random())
-    # calculating coordinates
-    x = r * math.cos(alpha) + circle_x
-    y = r * math.sin(alpha) + circle_y
-    return DataPoint(x, y)
-
-
-def create_point_around_y_line(x, func_y):
-    disp = np.random.uniform(-1, 1) + 1
-    return DataPoint(x, disp * func_y(x))
-
-
-def create_data(amount_of_points, func_y, interval_start_x=0, interval_end_x=100):
-    array = []
-
-    array += create_circle_of_points(int(amount_of_points / 2), -1, 70, 70, 25)
-    array += create_circle_of_points(int(amount_of_points / 2), 1, 30, 30, 25)
-
-    return array
-
-
-def create_circle_of_points(amount_of_points, type_class, circle_x, circle_y, circle_r):
-    array = []
-    for i in range(0, int(amount_of_points)):
-        point = creat_point_in_circle(circle_x, circle_y, circle_r)
-        point.class_type = type_class
-        array.append(point)
-    return array
 
 
 def normalize(points):
