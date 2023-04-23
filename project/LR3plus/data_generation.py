f23be4923301f1798dd80e8cd1a8f441f2a26de6
AFilonov <filon13p@gmail.com>
2023-04-23 16:01:46 +0300

    начал лр3

---
+++ project/LR3plus/data_generation.py
@@ -0,0 +1,71 @@
+import random
+from typing import Callable
+import numpy as np
+import math
+
+
+class DataPoint:
+    def __init__(self, x, y, class_type=0):
+        self.x = x
+        self.y = y
+        self.class_type = class_type
+
+    def __str__(self):
+        return "(" + str(self.x) + ":" + str(self.y) + ")"
+
+    def __repr__(self):
+        return str(self)
+
+
+def create_circle_of_points(amount_of_points, type_class, circle_x, circle_y, circle_r):
+    array = []
+    for i in range(0, int(amount_of_points)):
+        point = creat_point_in_circle(circle_x, circle_y, circle_r)
+        point.class_type = type_class
+        array.append(point)
+    return array
+
+
+def creat_point_in_circle(circle_x, circle_y, circle_r):
+    # random angle
+    alpha = 2 * math.pi * np.random.random()
+    # random radius
+    r = circle_r * math.sqrt(np.random.random())
+    # calculating coordinates
+    x = r * math.cos(alpha) + circle_x
+    y = r * math.sin(alpha) + circle_y
+    return DataPoint(x, y)
+
+
+def create_line_of_points(
+    amount_of_points: int, type_class: int, func_y: Callable[[float], float]
+):
+    array = []
+    for i in range(0, int(amount_of_points)):
+        point = create_point_around_y_line(i, func_y)
+        point.class_type = type_class
+        array.append(point)
+    return array
+
+
+def create_point_around_y_line(x: int, func_y: Callable[[float], float]):
+    disp = np.random.normal(0,300)
+    return DataPoint(x, disp + func_y(x))
+
+
+def normalize(points) -> list[DataPoint]:
+    array = []
+    max_x = max(point.x for point in points)
+    min_x = min(point.x for point in points)
+    max_y = max(point.y for point in points)
+    min_y = min(point.y for point in points)
+    for point in points:
+        array.append(
+            DataPoint(
+                (point.x - min_x) / (max_x - min_x),
+                (point.y - min_y) / (max_y - min_y),
+                point.class_type,
+            )
+        )
+
+    return array
--- project/LR3plus/main.py
+++ project/LR3plus/main.py
@@ -1,3 +1,74 @@
+import tensorflow as tf
+from keras.applications.densenet import layers
+import matplotlib.pyplot as plt
+
+from data_generation import *
+
+
+def create_plot(data: list[DataPoint], net) -> None:
+    x_vals, y_vals, color_vals = zip(
+        *[(float(i.x), float(i.y), i.class_type) for i in data]
+    )
+    plt.scatter(x_vals, y_vals, c=color_vals)
+
+    if not net is None:
+        x_line_vals = [(float(x) * 0.01) for x in range(1, 100, 1)]
+        y_line_vals = [(net.process([x, 1])) for x in x_line_vals]
+
+        plt.plot(x_line_vals, y_line_vals)
+
+    plt.xlim(min_lim, max_lim)
+    plt.ylim(min_lim, max_lim)
+    plt.show()
+
+
+def cubeFunction(x: float) -> float:
+    return 1*x * x - 100*x
+
+
+def squareFunction(x: float) -> float:
+    return x * x + 3 * x - 3
+
+
+def create_data(amount_of_points: int) -> list[DataPoint]:
+    array = []
+    array += create_line_of_points(amount_of_points, 1, squareFunction)
+    return array
+
+
+def prepare_data(need_normalize: bool):
+    global min_lim, max_lim, data
+    raw_data = create_data(100)
+    print(raw_data)
+    min_lim = 0
+    max_lim = 100
+    if need_normalize:
+        max_lim = 1
+        data = normalize(raw_data)
+    else:
+        data = raw_data
+    return data
+
+
+
 
 if __name__ == '__main__':
-    print("aaa")
+    model = tf.keras.Sequential([
+        # Добавляем полносвязный слой с 64 узлами к модели:
+        layers.Dense(1, activation=tf.keras.activations.sigmoid),
+        # Добавляем другой:
+        layers.Dense(1, activation='softmax'),
+        # Добавляем слой softmax с 10 выходами:
+        layers.Dense(10, activation='softmax')])
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(0.01),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    train_data = prepare_data(True)
+    test_data = prepare_data(True)
+
+    model.fit(data, labels, epochs=10, batch_size=32)
+
+
+    create_plot(data, None)
+
