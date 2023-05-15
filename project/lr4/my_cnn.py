f5df40aa3bb95ec36e81b557d04cd893583f5804
AFilonov <filon13p@gmail.com>
2023-05-15 16:15:53 +0300

    начал лр4

--- project/lr4/my_cnn.py
+++ project/lr4/my_cnn.py
@@ -1,10 +1,9 @@
 import os
+
 import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow
-from tensorflow.keras.datasets import mnist
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 
 current_dir = os.getcwd()
 (x_train, y_train), (x_test, y_test) = mnist.load_data(path=current_dir + '/mnist.npz')
@@ -61,4 +60,4 @@ score = model.evaluate(x_test, y_test_cat, verbose=0)
 print("Test score: %f" % score[0])
 print("Test accuracy: %f" % score[1])
 
-model.save('test_cnn_model')
+model.save('my_cnn_model')
